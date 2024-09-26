a64f5c57de61a1b88ed6fc90e831bf12fa7b3232
Kévin Bardoula <kevin@MacBook-Pro-de-Kevin.local>
2024-09-26 12:20:30 +0200

    étape 3 : récupérer les infos

--- main.c
+++ main.c
@@ -7,5 +7,16 @@ int main(){
 	int nb2;
 	char operateur;
 
+	/* Choix de l'opérateur */
+	printf("Bonjour ! Choisissez un opérateur parmi : +, -, *, /, %% : \n");
+	scanf("%s", operateur);
+
+	/* Choix du premier nombre */
+	printf("Choisissez un premier nombre entier : \n");
+	scanf("%d", &nb1);
+	/* Choix du second nombre */
+	printf("Choisissez votre second nombre : \n");
+	scanf("%d", &nb2);
+
 	exit(0);
 }
